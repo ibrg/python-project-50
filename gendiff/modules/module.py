1f7e9ebdbecd8e6f3d346f58870f4adb9fb53cab
ibrg <romanbezh@gmail.com>
2023-03-04 22:52:05 +0200

    refactored

--- gendiff/modules/module.py
+++ gendiff/modules/module.py
@@ -1,15 +1,9 @@
-import json
+from .parse import read_file
 
 
 UNCHANGE = ' '  # ключ есть в обоих файлах
 REMOVE = '-'  # находится в первом файле
 ADDED = '+'  # находится во втором файле
-
-
-def read_file(file, type=json):
-    with open(file) as f:
-        file = json.load(f)
-    return file
 
 
 def check_key_in_dicts(dict1: dict, dict2: dict, keys: list) -> dict:
@@ -21,10 +15,8 @@ def check_key_in_dicts(dict1: dict, dict2: dict, keys: list) -> dict:
             elif dict1[k] != dict2[k]:
                 result[REMOVE + ' ' + k] = {"value": dict1[k]}
                 result[ADDED + ' ' + k] = {"value": dict2[k]}
-
         elif k in dict1 and k not in dict2:
             result[REMOVE + ' ' + k] = {"value": dict1[k]}
-
         elif k not in dict1 and k in dict2:
             result[ADDED + ' ' + k] = {"value": dict2[k]}
     return result
